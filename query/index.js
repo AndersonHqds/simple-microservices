4b0c7fd944b0dac1f9919974b480e4adfa9b50c7
AndersonHqds <andyhhenrikefs@gmail.com>
2021-01-04 03:43:45 -0300

    Add: Scaffold to turn the develop mode more easy

--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -11,20 +11,20 @@ app.use(cors());
 const posts = {};
 
 const handleEvent = (type, data) => {
-  if (type === 'PostCreated') {
+  if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'CommentCreated') {
+  if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
-  if (type === 'CommentUpdated') {
+  if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
@@ -37,11 +37,11 @@ const handleEvent = (type, data) => {
   }
 };
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
   handleEvent(type, data);
@@ -50,12 +50,12 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4002, async () => {
-  console.log('Linstening on 4002');
+  console.log("Linstening on 4002");
 
-  const res = await axios.get('http://event-bus-srv:4005/events');
+  const res = await axios.get("http://event-bus-srv:4005/events");
 
   for (let event of res.data) {
-    console.log('Processing Event:', event.type);
+    console.log("Processing Event:", event.type);
 
     handleEvent(event.type, event.data);
   }
